4678f17f8820d893cf0bd0640d42879914260d35
Dell <Dell@DESKTOP-V6CN6AE>
2022-07-15 14:03:09 +0530

    Add project files.

---
+++ TicketsManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketsManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
